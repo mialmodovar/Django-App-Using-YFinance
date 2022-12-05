0dcf29bd60cf38a3d8abe90a19e065d0ef01dd44
Miguel Almodôvar <miguel.almodovar123@hotmail.com>
2022-12-05 08:38:14 +0000

    second commit

--- pages/urls.py
+++ pages/urls.py
@@ -8,6 +8,7 @@ path('search',views.search,name='search'),
 path('stock/<str:pk>',views.stock,name='stock'),
 path('get/stock', views.loadstock, name = "loadstock"),
 path('login',views.loginPage,name='login'),
+path('logout',views.logoutUser,name='logout'),
 path('register',views.registerPage,name='register')
 
     
