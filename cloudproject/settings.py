b2eed7196bee648a7fbefede6714720c0ad67e39
Miguel Almodôvar <miguel.almodovar123@hotmail.com>
2022-12-13 18:47:01 +0000

    bug fixes

--- cloudproject/settings.py
+++ cloudproject/settings.py
@@ -139,3 +139,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
