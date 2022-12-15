cf5c7397ebe7a85a80349a2aeaf8a3af7150be74
Miguel Almodôvar <miguel.almodovar123@hotmail.com>
2022-12-15 17:54:24 +0000

    production bug fixes

--- cloudproject/settings.py
+++ cloudproject/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
         'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
+        'PORT': '7983',
     }
 }
 
